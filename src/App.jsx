7ca5382c37f5794529b81e627fc0a94b0a809815
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-06-19 11:19:48 +0200

    test v elisa

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return <></>;
+	return <></>;
 }
 
 export default App;
--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,9 +1,9 @@
 const DetailPage = () => {
-  return (
-    <>
-      <h1>Detailpage</h1>
-    </>
-  );
+	return (
+		<>
+			<h1>Detailpage</h1>
+		</>
+	);
 };
 
 export default DetailPage;
