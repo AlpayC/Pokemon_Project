528075d2c298350090ea746154fca5f135def24e
AlpayC <alpaycelik91@googlemail.com>
2023-06-19 11:43:13 +0200

    take 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return <></>;
+  return (
+    <>
+      <h1>Aktuell</h1>
+    </>
+  );
 }
 
 export default App;
