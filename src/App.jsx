52dfbc4f3de18c9931ff6062ae536b6b0b34d10b
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-06-20 09:12:35 +0200

    first commit home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>blabl aa</h1>
-      <p>test</p>
+      <Home/>
     </>
   );
 }
