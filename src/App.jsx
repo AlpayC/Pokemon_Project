9012802635b8e9c3ebe14f2a5d13b3fbe3b5e2d8
AlpayC <alpaycelik91@googlemail.com>
2023-06-20 09:24:43 +0200

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>blabl aa</h1>
-      <p>test</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
